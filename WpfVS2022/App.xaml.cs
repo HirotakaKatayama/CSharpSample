22af9b72b027816e6957a86c9cf35b3467cef1f6
HirotakaKatayama <Hirotaka.Katayama@yokogawa.com>
2021-11-12 14:55:16 +0900

    VS2022新機能追加

---
+++ WpfVS2022/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.Windows;
+global using System.ComponentModel;
+
+namespace WpfVS2022;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ WpfVS2022/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Reactive.Bindings;
+
+namespace WpfVS2022;
+
+internal class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public ReactivePropertySlim<string> Name { get; }
+
+    public ViewModel()
+    {
+        Name = new ReactivePropertySlim<string>();
+    }
+}
